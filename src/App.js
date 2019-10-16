128ed58750965c1bf5a718695dcaba2c2e36bc51
spiano <lafleurpiano@gmail.com>
2019-10-16 12:27:09 -0500

    trying to loop through data to get an average temp for seven days

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import { Route, Link, Switch } from "react-router-dom";
 import Home from "./Home"
 
  class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            av: null
+        };
+        this.setAveTemp = this.setAveTemp.bind(this);
+    }
+    setAveTemp(aveTemp) {
+        this.setState({ av: aveTemp });
+    }
     render() {
         return (
             <div>
--- src/Home.js
+++ src/Home.js
@@ -3,30 +3,38 @@ import axios from "axios";
 
 const nasaURL = "https://api.nasa.gov/insight_weather/?api_key=ho6badIV7YVgrC00fo24mrl3juolIdryv8DPQsDc&feedtype=json&ver=1.0";
 
-export class Home extends Component {
+export default class Home extends Component {
     componentDidMount() {
+        const averageTemp = this.props.match.params.AT;
+        // const averageTemp = sol_keys.AT;
         const url = `${nasaURL}.json`;
-    
-    axios
-        .get(url)
-        .then(response => {
-            console.log(response);
-            // let season = response.data.insight_weather.season;
-            // this.props(season);
-            // response.json()
-        })
-        .catch(err => {
-            console.error(err);
-        });
+
+        axios
+            .get(url)
+            .then(response => {
+                console.log(response);
+               // console.log(Object.values(response.data)[0].AT.av);
+                let newAveTemp = Object.values(response.data);
+                //this.props.setAveTemp(newAveTemp);
+                // let season = response.data.insight_weather.season;
+                // this.props(season);
+                // response.json()
+                let weather = newAveTemp.map(avTemp => {
+                    console.log(avTemp);
+                })
+            })
+            .catch(err => {
+                console.error(err);
+            });
     }
     render() {
-        return (
-            <div>
-               <h1>Current Weather</h1>
-
-            </div>
-        )
-    }
+        // let weather = newAveTemp.map(avTemp => {
+            return (
+                <div key >
+                    <h1>Current Weather</h1>
+                    <h2>{this.weather}</h2>
+                        </div>
+           );
+         // })
+       }
 }
-
-export default Home;
